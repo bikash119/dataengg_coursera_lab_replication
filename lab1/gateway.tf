1765b23209ead0f5f42ba6715ff16e1caf978b20
bikash119 <bikashjpatra@gmail.com>
2024-11-14 08:38:00 +0530

    fixed the natgateway and internet gateway configuration

--- lab1/gateway.tf
+++ lab1/gateway.tf
@@ -6,7 +6,7 @@ resource "aws_internet_gateway" "lab1" {
   }
 }
 
-resource "aws_route_table" "ig_route" {
+resource "aws_route_table" "ig_route_a" {
   vpc_id = aws_vpc.lab1.id
 
   route {
@@ -15,18 +15,31 @@ resource "aws_route_table" "ig_route" {
   }
 
   tags = {
-    Name = "ig_route"
+    Name = "Internet Route Table AZ-A"
+  }
+}
+
+resource "aws_route_table" "ig_route_b" {
+  vpc_id = aws_vpc.lab1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.lab1.id
+  }
+
+  tags = {
+    Name = "Internet Route Table AZ-B"
   }
 }
 
 resource "aws_route_table_association" "public_a" {
   subnet_id      = aws_subnet.public_subnet_a.id
-  route_table_id = aws_route_table.ig_route.id
+  route_table_id = aws_route_table.ig_route_a.id
 }
 
 resource "aws_route_table_association" "public_b" {
   subnet_id      = aws_subnet.public_subnet_b.id
-  route_table_id = aws_route_table.ig_route.id
+  route_table_id = aws_route_table.ig_route_b.id
 }
 ## Create NAT Gateway to allow outbound internet traffic from private subnet a
 resource "aws_nat_gateway" "lab1_a" {
@@ -42,7 +55,7 @@ resource "aws_nat_gateway" "lab1_b" {
   connectivity_type = "private"
   subnet_id         = aws_subnet.public_subnet_b.id
   tags = {
-    Name = "Lab1 NAT Gateway A"
+    Name = "Lab1 NAT Gateway B"
   }
 }
 
@@ -56,24 +69,34 @@ resource "aws_eip" "nat_eip_b" {
   vpc = true
 }
 
-resource "aws_route_table" "private_subnets"{
+resource "aws_route_table" "private_a"{
   vpc_id = aws_vpc.lab1.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_nat_gateway.lab1_a.id
   }
-
   tags = {
-    Name = "private_subnets"
+    Name = "Private Route Table AZ-A"
+  }
+}
+
+resource "aws_route_table" "private_b"{
+  vpc_id = aws_vpc.lab1.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.lab1_b.id
+  }
+  tags = {
+    Name = "Private Route Table AZ-B"
   }
 }
 
 resource "aws_route_table_association" "private_subnet_a" {
   subnet_id      = aws_subnet.private_subnet_a.id
-  route_table_id =  aws_route_table.private_subnets.id
+  route_table_id =  aws_route_table.private_a.id
 }
 
 resource "aws_route_table_association" "private_subnet_b"{
   subnet_id       = aws_subnet.private_subnet_b.id
-  route_table_id  = aws_route_table.private_subnets.id
+  route_table_id  = aws_route_table.private_b.id
 }
