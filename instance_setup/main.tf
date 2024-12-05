66736f34cef24799a16c2f45ab617bee29fab58e
bikash119 <bikashjpatra@gmail.com>
2024-12-05 08:58:47 +0530

    modular approach to building infrastructure using terraform modules

---
+++ instance_setup/main.tf
@@ -0,0 +1,21 @@
+
+# Declare the data source
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.16.0"
+
+  cidr = var.vpc_cidr_block
+
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = slice(var.private_subnet_cidr_blocks, 0, var.private_subnet_count)
+  public_subnets  = slice(var.public_subnet_cidr_blocks, 0, var.public_subnet_count)
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+
+  map_public_ip_on_launch = false
+}
--- lab1/policies.tf
+++ lab1/policies.tf
@@ -30,12 +30,10 @@ data "aws_iam_policy_document" "instance_connect"{
 data "aws_iam_policy_document" "ec2_assume_role" {
   statement {
     effect = "Allow"
-
     principals {
       type        = "Service"
       identifiers = ["ec2.amazonaws.com"]
     }
-
     actions = ["sts:AssumeRole"]
   }
 }
